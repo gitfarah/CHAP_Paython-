ad4895ef7e321e261b5cc358b26559679e9f379d
gitfarah <52824788+gitfarah@users.noreply.github.com>
2020-08-27 09:30:08 +0200

    random number generator added

--- server.py
+++ server.py
@@ -1,1 +1,38 @@
 
+from xmlrpc.server import SimpleXMLRPCServer
+from xmlrpc.server import SimpleXMLRPCRequestHandler
+
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+server = SimpleXMLRPCServer(("localhost", 8000),
+                            requestHandler=RequestHandler)
+server.register_introspection_functions()
+
+# Generate random number 
+randnum = [1,2,3,4,5,6,7]
+random.shuffle(randnum)
+
+
+# Register pow() function; this will use the value of
+# pow.__name__ as the name, which is just 'pow'.
+server.register_function(pow)
+
+# Register a function under a different name
+def adder_function(x,y):
+    return x + y
+server.register_function(adder_function, 'add')
+
+# Register an instance; all the methods of the instance are
+# published as XML-RPC methods (in this case, just 'div').
+class MyFuncs:
+    def div(self, x, y):
+        return x // y
+
+server.register_instance(MyFuncs())
+
+# Run the server's main loop
+server.serve_forever()
