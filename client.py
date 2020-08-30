eaf49bde6624eed708907ceae5af959bb746d6eb
omar.farah <omar.farah@deutschebahn.com>
2020-08-30 16:10:25 +0200

    client password entering stage improved

--- client.py
+++ client.py
@@ -1,26 +1,13 @@
 import xmlrpc.client
-import hashlib
 
 
 
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
-password = 'Pass123'
-key = hashlib.sha256(s.genRandnumber().encode('utf-8'))
 
-def clientHash():
+print(s.hasher(input('enter Password: ')))
 
-    return password + key.hexdigest()
-
-
-print(clientHash())
+#print(s.add(2,2))
 
 
 
-
-# failed operation:
-'''
-token = password + s.genRandnumber()
-key = hashlib.blake2b()
-key.update(b'token')
-'''
 
