4372a009c9def1d17cf537b50d81cbb443c556d9
gitfarah <52824788+gitfarah@users.noreply.github.com>
2020-08-31 22:02:31 +0200

    server.py updated

--- server.py
+++ server.py
@@ -1,20 +1,20 @@
-
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 import random
 import string
 import hashlib
-import sys
 
 
 # Restrict to a particular path.
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
+
 # Create server
 server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=RequestHandler, allow_none=True)
 server.register_introspection_functions()
+
 
 # Generate random number
 def genRandnumber(size=6, chars=string.ascii_uppercase + string.digits):
@@ -22,69 +22,38 @@ def genRandnumber(size=6, chars=string.ascii_uppercase + string.digits):
 	return ''.join(random.choice(chars) for _ in range(size))
 server.register_function(genRandnumber)
 
-# Create server hasher
+users = {"omar" : "pass123", "jan" : "pass456" }
+rand = genRandnumber()
+
+
+# method hasher() erstellt hashWert:
+def hasher(random, password):
+    key = random + password
+    hashValue = hashlib.sha256(key.encode('utf-8'))
+    return hashValue.hexdigest()
+
+
+
+
+
+
+
+
+'''
 def hasher(password):
     if password == 'Pass123':
         key = hashlib.sha256(genRandnumber().encode('utf-8'))
         password + key.hexdigest()
-        return "you're logged in!"
+        return "you're logged int!"
 
     elif password != 'Pass123':
-        return "loggin failed!"
-	sys.exit(0)
+        return "logging failed!", server.server_close()
+
+
+
 server.register_function(hasher)
 
-
-# Notes to improve hasher 
 '''
-Users = {Omar, Max}
-
-# Create clients()
-def clients():
-	login = input("Enter username: ")
-	
-	#check!
-	if login in Users:
-		clientHasher()
-		
-	
-
-
-# Create serverHasher()
-def serverHasher():
-		password = 'Pass123':
-		hash = password + genRandnumber()
-		hashlib.sha256(hash.encode('utf-8'))
-		return hash.hexdigest()
-#server.register_function(hasher)
-
-# Create clientHasher()
-def clientHasher(password):
-	if password == 'Pass123':
-		hash = password + genRandnumber()
-		hashlib.sha256(hash.encode('utf-8'))
-		return hash.hexdigest()
-#server.register_function(serverHasher)
-'''
-
-# Notes for hashCompare
-'''
-def hashCompare():
-    while (true):
-	if serverHasher() == clientHasher():
-		return "you're logged in"
-		break
-	elif serverHasher() != clientHasher():
-		return "loggin failed, plz repeat"
-	elif serverHasher() != clientHasher():
-		return "loggin failed, plz repeat"
-	elif serverHasher() != clientHasher():
-		return "loggin failed, plz repeat"
-	else:
-		sys.exit(0)
-		
-'''
-
 
 # test function
 def add(x, y):
